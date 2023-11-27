1717f04bca5e207c4497fda049815262a7b55c51
Nicholas Franck <97461306+salohcin98@users.noreply.github.com>
2023-11-27 18:18:21 -0500

    ARM code

---
+++ part6-breakout/io.h
@@ -0,0 +1,16 @@
+#ifndef IO_H
+#define IO_H
+
+#define PERIPHERAL_BASE 0x3F000000
+#define SAFE_ADDRESS    0x00210000 // Somewhere safe to store a lot of data
+
+void uart_init();
+void uart_writeText(char *buffer);
+void uart_loadOutputFifo();
+unsigned char uart_readByte();
+unsigned int uart_isReadByteReady();
+void uart_writeByteBlocking(unsigned char ch);
+void uart_update();
+void mmio_write(long reg, unsigned int val);
+unsigned int mmio_read(long reg);
+#endif 
