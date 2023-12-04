68a6fc260230ee8c33a2e8e1de645e9e658d8be6
ChasePoland0121 <31477538+Chasinggoodgrades@users.noreply.github.com>
2023-12-04 11:32:19 -0500

    Update kernel.cpp

--- Tic-Tac-Toe/kernel.cpp
+++ Tic-Tac-Toe/kernel.cpp
@@ -1,9 +1,10 @@
-//
-// kernel.cpp
-//
-// Circle - A C++ bare metal environment for Raspberry Pi
-// Copyright (C) 2014-2020  R. Stange <rsta2@o2online.de>
-// 
+// This file includes code from the Circle library:
+//   Circle - A C++ bare metal environment for Raspberry Pi
+//   Copyright (C) 2014-2023  R. Stange <rsta2@o2online.de>
+
+// Additional modifications and enhancements have been made
+// by [Chase Poland, Nicholas Franck, & Eric Peasley] as part of the [Franckenator Productions] project.
+
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
